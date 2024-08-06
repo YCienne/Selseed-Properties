c4f46834a5e363204b8ce8055280e7111f65db98
YCienne <lucienneawoudi23@gmail.com>
2024-08-06 12:53:54 +0000

    B

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./Components/Benefits";
+import Collaboration from "./Components/Collaboration";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import Services from "./Components/Services";
 
 const App = () => {
   
@@ -10,6 +13,9 @@ const App = () => {
     <div className="pt-[5.75rem] lg:pt-[1.25rem] overflow-hidden">
       <Header />
       <Hero />
+      <Benefits />
+      <Collaboration />
+      <Services />
     </div>
 
     <ButtonGradient />
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,16 @@
 import Section from "./Section"
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogo from "./CompanyLogo";
 
 const Hero = () => {
+    const parallaxRef = useRef(null);
   return (
     <Section 
     className="pt-[12rem] -mt-[5.25]"
@@ -11,7 +19,7 @@ const Hero = () => {
     customPaddings
     id="hero"
     > 
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[1.875rem] md:mb-10 lg:mb-[6.25rem] lg:mt-[6.25rem]" >
             <h1 className="h1 mb-6">
                 Explore Possibilities&nbsp;with&nbsp; {' '}
@@ -38,8 +46,32 @@ const Hero = () => {
                     <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]"></div>
                     <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                     <img src={robot} className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]" alt="AI"/>
+                    
+                    
+                    < Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto
+                    md:bottom-8 md:w-[31rem] md:-translate-x-1/2 xl:flex"  />
+                    
+
+                    <ScrollParallax isAbsolutelyPositioned>
+                        <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1
+                        bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                            {heroIcons.map((icon, index) => (
+                                <li className="p-5" key={index}>
+                                    <img src={icon} width={24} height={25} alt={icon} />
+                                </li>
+                            ))}
+                        </ul>
+                        <BackgroundCircles />
+                    </ScrollParallax>
+
+                    <ScrollParallax isAbsolutelyPositioned>
+                        <Notification className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                        title="Code Generation" />
+                    </ScrollParallax>
                     </div>
                 </div>
+
+                <Gradient />
             </div>
             <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
                 <img src={heroBackground} 
@@ -48,9 +80,14 @@ const Hero = () => {
                 height={1800} 
                 alt="hero"/>
             </div>
+            // BackgroundCircles was here
         </div>
+
+        <CompanyLogo className="hidden relative z-10 mt-20 lg:block" />
+
       </div>
-    </Section>
+      <BottomLine />
+    </Section> 
   )
 }
 
